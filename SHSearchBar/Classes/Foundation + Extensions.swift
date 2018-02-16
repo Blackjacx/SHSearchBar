36f73a4627faa7a6136b9db150ce14ec9ca69878
Stefan Herold <stefan.herold@gmail.com>
2018-02-16 01:12:00 +0100

    Added file to project

--- SHSearchBar/Classes/Foundation + Extensions.swift
+++ SHSearchBar/Classes/Foundation + Extensions.swift
@@ -11,7 +11,7 @@ public extension String {
 
     private class AFrameworkClass {}
 
-    var localized: String {
+    public var localized: String {
         let appTranslation = NSLocalizedString(self, comment: "")
 
         if appTranslation != self {
